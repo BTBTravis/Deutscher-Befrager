ec8ba5408d3d0f110986df90bf28a9748a2259c8
Travis Shears <btbtravis@gmail.com>
2019-05-01 15:05:19 +0200

    Bring answer command back online

--- interviewer/core.py
+++ interviewer/core.py
@@ -1,6 +1,7 @@
 import click
 import interviewer.store as store
 import interviewer.util as utils
+import interviewer.answerer as answerer
 from sys import exit
 
 # @click.command()
@@ -17,7 +18,26 @@ def cli():
 
 @click.command()
 def answer():
-    click.echo('TODO: parse answer')
+    """Retrive a question to answer"""
+    local_store = store.load_store()
+    next_key = answerer.next_question(local_store)
+    try:
+        answer = input(local_store[next_key]['Q'] + "\n")
+    except:
+        exit(1)
+
+    if 'A' not in local_store[next_key]:
+        local_store[next_key]['A'] = [answer]
+    else:
+        local_store[next_key]['A'].append(answer)
+
+    try:
+        store.save_store(local_store)
+    except:
+        click.echo("Could not save answer to store")
+    click.echo(f"Added {answer} to store")
+
+
 
 @click.command()
 @click.argument('question')
