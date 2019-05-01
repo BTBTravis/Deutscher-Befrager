e7279f7cf690b7a1d7c38458f8da85a4c2e73e5f
Travis Shears <btbtravis@gmail.com>
2019-05-01 18:07:36 +0200

    Modify how we export TRAVIS_JOB_ID

--- buttons.sh
+++ buttons.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 printf "Copying CI_JOB_ID: ""$CI_JOB_ID"" to TRAVIS_JOB_ID\n"
-export TRAVIS_JOB_ID="$CI_JOB_ID"
-printf "TRAVIS_JOB_ID: ""$TRAVIS_JOB_ID"
+TRAVIS_JOB_ID="$CI_JOB_ID"
+export TRAVIS_JOB_ID
+printf "TRAVIS_JOB_ID: ""$TRAVIS_JOB_ID""\n"
 
 coveralls
